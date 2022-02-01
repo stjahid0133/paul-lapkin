fba7d36ca9afab160d3c2da9e0c301a9a050dc09
stjahid0133 <dj420jahid@gmail.com>
2022-02-01 22:56:38 +0600

    first commit

---
+++ resourese/js/script.js
@@ -0,0 +1,1 @@
+var mixer=mixitup('.protfolio_bar_container'); $(function(){$.scrollify({section : ".scrollify", setHeights: false, updateHash: false,});});// manu barfunction openMenu(){document.getElementById('nav_bar').style.height="100%";}function closeMenu(){document.getElementById('nav_bar').style.height="0%";}
